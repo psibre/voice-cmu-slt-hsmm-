82f6adb83f2facbb266c9f99ff28019732e95249
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-02-17 13:46:43 +0100

    migrate to gradle-voicebuilding-plugin

---
+++ voice.groovy
@@ -0,0 +1,14 @@
+voice {
+    name = 'cmu-slt-hsmm'
+    language = 'en'
+    region = 'US'
+    gender = 'female'
+    type = 'hsmm'
+    description = 'A female US English hidden semi-Markov model voice, built from recordings provided by Carnegie Mellon University'
+    license {
+        name = 'Arctic License'
+        shortName = 'Arctic'
+        url = 'http://mary.dfki.de/download/arctic-license.html'
+    }
+    samplingRate = 16000
+}
